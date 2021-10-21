e7cb83e2efc2b5ff052bb75f83dcecb27f539902
Vladislav_Ivanov <55939441+Ivanov-Vladislav@users.noreply.github.com>
2021-10-21 16:19:32 +0300

    Delete Common directory

--- Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace Common
-{
-    public static class Constants
-    {
-        public const string TextKeyPrefix = "TEXT-";
-        public const string RankKeyPrefix = "RANK-";
-        public const string SimilarityKeyPrefix = "SIMILARITY-";
-    }
-}
