425076b0cde6cead9ecb406bf08146b45d523789
Vladislav_Ivanov <55939441+Ivanov-Vladislav@users.noreply.github.com>
2021-10-21 16:24:50 +0300

    upload pa6

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Client
+{
+    class Program
+    {
+        private const string EndOfMessage = "<EOF>";
+        public static void StartClient(string address, int port, string message)
+        {
+            try
+            {
+                IPAddress ipAddress = address == "localhost" ? IPAddress.Loopback : IPAddress.Parse(address);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // CREATE
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // CONNECT
+                    sender.Connect(remoteEP);
+
+                    byte[] text = Encoding.UTF8.GetBytes(message + EndOfMessage);
+
+                    // SEND
+                    int bytesSent = sender.Send(text);
+
+                    // RECEIVE                   
+                    byte[] buf = new byte[1024];
+                    string historyStr = null;
+                    int bytesRec = 0;
+
+                    do
+                    {
+                        bytesRec = sender.Receive(buf);
+                        historyStr += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                    }
+                    while (bytesRec > 0);
+
+                    var history = JsonSerializer.Deserialize<List<string>>(historyStr);
+
+                    foreach (var msg in history)
+                    {
+                        Console.WriteLine(msg);
+                    }
+
+                    // RELEASE
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new ArgumentException("Invalid arguments count");
+            }
+
+            StartClient(args[0], Int32.Parse(args[1]), args[2]);
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Server
+{
+    class Program
+    {
+        private const string EndOfMessage = "<EOF>";
+        public static void StartListening(string port)
+        {
+            IPAddress ipAddress = IPAddress.Any; 
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, Convert.ToInt32(port));
+            List<string> messageHistory = new List<string>();
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    
+                    byte[] buf = new byte[1024];
+                    string data = null;
+                    while (true)
+                    {
+                        // RECEIVE
+                        int bytesRec = handler.Receive(buf);
+
+                        data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                        if (data.IndexOf(EndOfMessage) > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    data = data.Remove(data.Length - 5, 5);
+                    messageHistory.Add(data);
+
+                    Console.WriteLine("Message received: {0}", data);
+
+                    string historyJson = JsonSerializer.Serialize(messageHistory);
+                    byte[] history = Encoding.UTF8.GetBytes(historyJson);
+                    handler.Send(history);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("Invalid arguments count");
+            }
+
+            StartListening(args[0]);
+        }
+    }
+}
